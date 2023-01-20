dcfda7ecddb6dc243c281168904d440da3e319b6
Cook1619 <cook6348@gmail.com>
2023-01-19 20:36:09 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Diagnostics;
+using CSharpPlayground;
+
+Console.WriteLine("Creating an employee");
+
+Employee danelle = new Employee("Danelle", "Cook", "dcook@gmail.com", new DateTime(1982, 1, 23), 25);
+
+danelle.DisplayEmployeeDetails();
+
+danelle.PerformWork();
+danelle.PerformWork();
+danelle.PerformWork(5);
+danelle.PerformWork();
+
+double receivedWageDanelle = danelle.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message from program): {receivedWageDanelle}");
