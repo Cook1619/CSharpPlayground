8c3221934e8bce130322b43b1eaee00137452162
Cook1619 <cook6348@gmail.com>
2023-01-19 20:39:13 -0600

    Adding more objects

--- Program.cs
+++ Program.cs
@@ -15,3 +15,16 @@ danelle.PerformWork();
 
 double receivedWageDanelle = danelle.ReceiveWage(true);
 Console.WriteLine($"Wage paid (message from program): {receivedWageDanelle}");
+
+Employee matt = new Employee("Matt", "Cook", "mcook@gmail.com", new DateTime(1986, 1, 23), 50);
+
+matt.DisplayEmployeeDetails();
+
+matt.PerformWork();
+matt.PerformWork();
+matt.PerformWork(10);
+matt.PerformWork();
+
+double receivedWageMatt = matt.ReceiveWage(true);
+
+Console.WriteLine($"Wage paid (message from program): {receivedWageMatt}");
